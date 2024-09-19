af9de9d34932229bc5b248f3f4dbae7addf1c4bb
Munkhdorj-M <64317250+munkhdorj-m@users.noreply.github.com>
2024-09-19 12:25:44 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,26 +1,26 @@
 # You can remove 'pass' if you written code in the function 
 
 # Exercise 1
-def greet():
+def find_lesser_of_two_numbers(num1, num2):
     # Write your code here
     pass
 
 # Exercise 2
-def perform_operations(a, b):
+def sum_of_numbers_not_divisible_by_11(numbers):
     # Write your code here
     pass
 
 # Exercise 3
-def check_even_odd(n):
+def find_largest_digit_of_3_digit_number(number):
     # Write your code here
     pass
 
 # Exercise 4
-def compare_numbers(num1, num2):
+def check_number_greater_than_10(number):
     # Write your code here
     pass
 
 # Exercise 5
-def multiply_digits(num):
+def convert_number_to_letter_rating(number):
     # Write your code here
     pass
